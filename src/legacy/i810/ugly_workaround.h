7c891696ee3aa9839b999e2ea616c19b0ad36af4
nullifed <nol@cock.li>
2024-04-24 11:33:04 +0200

    i810: Fix compilation errors.
    I will hook up symbol detection into the build system later on.

--- src/legacy/i810/ugly_workaround.h
+++ src/legacy/i810/ugly_workaround.h
@@ -1,21 +1,18 @@
 #include "vbe.h"
 #include "vgaHW.h"
 
-void vbeFree(vbeInforPtr pVbe) __attribute__((weak));
-void vgaHWProtectProc(ScrnInfoPtr pScrn, Bool on) __attribute__((weak));
-
 void _vbeFree(vbeInfoPtr pVbe) {
-	if (vbeFree) {
+#if defined (HAS_VBE_FREE)
 		vbeFree(pVbe);
-	} else {
+#else
 		abort();
-	}
+#endif
 }
 
 void _vgaHWProtectProc(ScrnInfoPtr pScrn, Bool on) {
-	if (vgaHWProtectProc) {
+#if defined (HAS_VGA_HW_PROTECT)
 		vgaHWProtectProc(pScrn, on);
-	} else {
+#else
 		abort();
-	}
+#endif
 }
