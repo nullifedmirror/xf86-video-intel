6e4ef2d76d7133241e664b9350a065246514e5af
nullifed <nol@cock.li>
2024-04-24 11:25:52 +0200

    i810: Workaround more symbol packing issues

--- src/legacy/i810/ugly_workaround.h
+++ src/legacy/i810/ugly_workaround.h
@@ -1,9 +1,21 @@
 #include "vbe.h"
+#include "vgaHW.h"
+
+void vbeFree(vbeInforPtr pVbe) __attribute__((weak));
+void vgaHWProtectProc(ScrnInfoPtr pScrn, Bool on) __attribute__((weak));
 
 void _vbeFree(vbeInfoPtr pVbe) {
-#if defined(HAS_VBE_FREE)
-	vbeFree(pVbe);
-#else
-	abort();
-#endif
+	if (vbeFree) {
+		vbeFree(pVbe);
+	} else {
+		abort();
+	}
+}
+
+void _vgaHWProtectProc(ScrnInfoPtr pScrn, Bool on) {
+	if (vgaHWProtectProc) {
+		vgaHWProtectProc(pScrn, on);
+	} else {
+		abort();
+	}
 }
